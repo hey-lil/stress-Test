0837a98b303050db4b31568ae29a5fcebd5839a6
Student <example@example.com>
2020-01-23 10:59:26 -0800

    add CSS styling for a gradient background, add initial checkbox form in HTML and JS file

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    $("").show();
+    $("").hide();
+  });
+
+  var signs = [];
+  var symptoms = [];
+  var methods = [];
+
+  var blanks = 
+
+
+
+});
