1c22796195c88c210b0f8b40c3af70f63d89871e
Christine Augustine and Lillian Taft <christine+lillian@>
2020-01-23 12:01:14 -0800

    add js .each functions to return value of checked ansers

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("form#stress").submit(function(event) {
     event.preventDefault();
-    $("").show();
-    $("").hide();
+ 
+
+    $("#veryStressed0").show();
+    $("input:checkbox[name=signs]:checked").each(function() {
+      var checkedSigns = $(this).val();
+      $("#veryStressed0").append(checkedSigns + "<br>");
+    });
+    $("veryStressed1").show();
+    $("input:checkbox[name=symptoms]:checked").each(function() {
+      var checkedSymptoms = $(this).val();
+      $("#veryStressed1").append(checkedSymptoms + "<br>");
+    });
+  
+  
+  
   });
-
-  var signs = [];
-  var symptoms = [];
-  var methods = [];
-
-  var blanks = 
-
-
-
 });
